55925ddce7dfd8c83ebf2eef32cee82c79649aae
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-08 16:38:59 +0530

    upload

---
+++ routes/additionalFieldsRoutes.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import multer from 'multer';
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+import {
+    addField,
+    listFields,
+    deleteField 
+} from '../controllers/additionalFieldsController.js'; // Adjust the path according to your project structure
+
+//Middleware
+//import userAuth from '../middleware/authMiddleware.js';
+//import Companymid from '../middleware/companyMiddleware.js';
+
+
+// Initialize dotenv to load environment variables
+dotenv.config();
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Initialize router
+const userRouter = express.Router();
+
+// Setup multer with memory storage for handling file uploads
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+userRouter.post('/add_fields', upload.none(), addField);
+userRouter.post('/list_fields', upload.none(), listFields);
+userRouter.post('/delete_fields',  upload.none(), deleteField);
+
+export default userRouter;
