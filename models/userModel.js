b11d9b8aa851c2d75e23606bb92dadf2f991d6a7
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-10 12:15:46 +0530

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,60 +1,73 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    allowed_verifications :{
-    type: String,
-    },
-    
-    
-    password: {
-        type: String,
-        required: true,
-    },
-    profilePicture: {
-        type: String,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    gender: {
-        type: String,
-    },
-    transaction_fee: {
-        type: String,
-    },
-    transaction_gst: {
-        type: String,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now,
-    },
-    is_del: {
-        type: Boolean,
-        default: false,
-    },
-    is_active: {
-        type: Boolean,
-        default: true,
-    },
-    role: {
-        /* 0 for admin ,1 for candidate ,2 for company  */
-        type: Number,
-        required: true,
-        default: 1,
-    }
+name: {
+type: String,
+required: true,
+},
+password: {
+type: String,
+required: true,
+},
+profilePicture: {
+type: String,
+},
+email: {
+type: String,
+required: true,
+unique: true,
+},
+phone_number: {
+type: String,
+},
+address: {
+type: String,
+},
+gst_no: {
+type: String,
+},
+package_id: {
+type: String,
+},
+allowed_verifications :{
+type: String,
+},
+discount_percent: {
+type: String,
+},   
+gender: {
+type: String,
+},
+transaction_fee: {
+type: String,
+},
+transaction_gst: {
+type: String,
+},
+createdAt: {
+type: Date,
+default: Date.now,
+},
+updatedAt: {
+type: Date,
+default: Date.now,
+},
+is_del: {
+type: Boolean,
+default: false,
+},
+is_active: {
+type: Boolean,
+default: true,
+},
+role: {
+/* 0 for admin ,1 for candidate ,2 for company  */
+type: Number,
+required: true,
+default: 1,
+}
 }, {
-    timestamps: true,
+timestamps: true,
 });
 
 const User = mongoose.model("User", userSchema);
