a6b82cc025b489ddb3b7d2a91db5d9280efd8839
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-11 13:11:51 +0530

    Add files via upload

---
+++ routes/walletRoute.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import multer from 'multer';
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+import {
+    addTransaction
+} from '../controllers/walletController.js'; // Adjust the path according to your project structure
+
+//Middleware
+import userAuth from '../middleware/authMiddleware.js';
+import Companymid from '../middleware/companyMiddleware.js';
+
+
+// Initialize dotenv to load environment variables
+dotenv.config();
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Initialize router
+const userRouter = express.Router();
+
+// Setup multer with memory storage for handling file uploads
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+userRouter.post('/addTransaction', upload.none(), userAuth, Companymid, addTransaction);
+// userRouter.get('/list_user_cart', upload.none(), userAuth, Companymid, getUserVerificationCartByEmployer);
+
+
+export default userRouter;
