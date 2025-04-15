f836e464521032e4f0ad5c4afcbc3eb013e68e58
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-15 15:52:25 +0530

    Update packageModel.js

--- models/packageModel.js
+++ models/packageModel.js
@@ -1,1 +1,16 @@
+import mongoose from "mongoose";
+
+const packageSchema = new mongoose.Schema({
+  name: { type: String },
+  transaction_fee: { type: Number },
+  description: { type: String },
+  transaction_gst: { type: Number, default: 18 },
+  allowed_verifications: { type: [String], default: [] },
+  expiryDate: { type: Date },
+  is_del: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Package = mongoose.model('Package', packageSchema);
+export default Package;
 
