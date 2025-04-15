2e44e1d21709babd239484ceddb8bc9091f972a4
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-15 16:17:29 +0530

    Update packageModel.js

--- models/packageModel.js
+++ models/packageModel.js
@@ -5,7 +5,10 @@ const packageSchema = new mongoose.Schema({
   transaction_fee: { type: Number },
   description: { type: String },
   transaction_gst: { type: Number, default: 18 },
-  allowed_verifications: { type: [String], default: [] },
+  allowed_verifications: {
+    type: [String], // Array of strings like ["PAN", "AADHAAR"]
+    default: [],
+  },
   expiryDate: { type: Date },
   is_del: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
