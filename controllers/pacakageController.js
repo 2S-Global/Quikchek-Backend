1279154a2d116e5d1fa80ffe2ff689a7eafbfa21
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-15 15:39:57 +0530

    Add files via upload

---
+++ controllers/pacakageController.js
@@ -0,0 +1,29 @@
+import userPackage from "../models/packageModel.js";
+
+export const addPackage = async (req, res) => {
+    try {
+      const {
+        name,
+        transaction_fee,
+        description,
+        transaction_gst,
+        allowed_verifications,
+        expiryDate,
+      } = req.body;
+  
+      const newPackage = new userPackage({
+        name,
+        transaction_fee,
+        description,
+        transaction_gst: transaction_gst || 18,
+        allowed_verifications,
+        expiryDate,
+      });
+  
+      const savedPackage = await newPackage.save();
+      res.status(201).json({ message: 'Package added successfully', data: savedPackage });
+    } catch (error) {
+      res.status(500).json({ message: 'Error adding package', error: error.message });
+    }
+  };
+  
