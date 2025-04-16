928b501c4167960b2d3709a00e09ca737416e0d7
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-16 12:23:58 +0530

    Add files via upload

---
+++ routes/companyPackageRoute.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import multer from 'multer';
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+import { createCompanyPackage } from '../controllers/companyPackageController.js';
+
+//Middleware
+import userAuth from '../middleware/authMiddleware.js';
+import Companymid from '../middleware/companyMiddleware.js';
+
+
+// Initialize dotenv to load environment variables
+dotenv.config();
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Initialize router
+const userRouter = express.Router();
+
+// Setup multer with memory storage for handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// userRouter.post('/list_verified_users', upload.none(), userAuth, Companymid, listUserVerifiedList);
+
+
+userRouter.post("/createCompanyPackage",upload.none(), createCompanyPackage);
+
+
+export default userRouter;
