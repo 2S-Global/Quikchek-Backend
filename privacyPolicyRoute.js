2928006fddde8c5fd0508fcfc51197364651b997
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-17 16:07:27 +0530

    Delete privacyPolicyRoute.js

--- privacyPolicyRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPrivacyPolicy } from '../controllers/privacyPolicyController.js';
-
-const userRouter = express.Router();
-
-// Route to get the privacy policy
-userRouter.get("/getPrivacyPolicy", getPrivacyPolicy);
-
-export default userRouter;
