e0ca330e483e2511d8206ddcb5b0a5624a52e58d
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-17 12:10:45 +0530

    Add files via upload

---
+++ routes/termsRoutes.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import multer from 'multer';
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+import { getTerms } from '../controllers/termsController.js';
+
+//Middleware
+import userAuth from '../middleware/authMiddleware.js';
+import Companymid from '../middleware/companyMiddleware.js';
+
+
+// Initialize dotenv to load environment variables
+dotenv.config();
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Initialize router
+const userRouter = express.Router();
+
+// Setup multer with memory storage for handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// userRouter.post('/list_verified_users', upload.none(), userAuth, Companymid, listUserVerifiedList);
+
+
+
+userRouter.get("/getTerms", getTerms);
+
+
+export default userRouter;
