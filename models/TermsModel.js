cfcfbfc596f35c1274dcc0f5f4d6e4900ecc354f
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-17 12:13:10 +0530

    Add files via upload

---
+++ models/TermsModel.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const termsSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Terms = mongoose.model('Terms', termsSchema);
+
+export default Terms;
