13364fb50ec5ed60f012254cd2f3d547988f94ce
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-17 12:21:28 +0530

    Update termsController.js

--- controllers/termsController.js
+++ controllers/termsController.js
@@ -10,8 +10,10 @@ export const getTerms = async (req, res) => {
     if (!terms) {
       return res.status(200).json({ message: 'No terms found' });
     }
+const cleanContent = terms.content.replace(/<[^>]*>/g, '');
 
-    res.status(200).json({ terms });
+    res.status(200).json({ terms: { content: cleanContent } });
+
   } catch (error) {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
