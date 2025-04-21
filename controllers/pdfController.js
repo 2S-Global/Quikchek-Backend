90762d4daab3a58b76e4f56942447b4e1f697c00
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-21 13:43:22 +0530

    pdf dynamin pan , adhar , passport

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -41,244 +41,183 @@ export const generatePDF = async (req, res) => {
         const year = dobDate.getFullYear();
         const formattedDob = `${day}/${month}/${year}`;
         const phone = user.candidate_mobile;
+        const Email = user.candidate_email;
+        const Gender = user?.candidate_gender ?? "N/A";
+
+        const verification_time = new Date(user.createdAt).toLocaleString(
+          "en-IN",
+          {
+            timeZone: "Asia/Kolkata",
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: true, // for AM/PM format
+          }
+        );
 
         // **HTML Content with Bootstrap**
-        const htmlContent = `
-  <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>User Information</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-  </head>
-  <body>
-    <div class="container mt-2 small">
-      <h2 class="text-primary">User Information</h2>
-      <div class="row g-1">
-        <div class="col-md-6">
-          <div class="mb-1"><strong>Full Name:</strong> ${userName}</div>
-        </div>
-        <div class="col-md-6">
-          <div class="mb-1"><strong>Date Of Birth:</strong> ${formattedDob}</div>
-        </div>
-        <div class="col-md-6">
-          <div class="mb-1"><strong>Phone Number:</strong> ${phone}</div>
-        </div>
-        <div class="col-md-6">
-          <div class="mb-1"><strong>Email:</strong> abc@gmail.com</div>
-        </div>
-        <div class="col-md-6">
-          <div class="mb-1"><strong>Address:</strong> KOLKATA</div>
-        </div>
-        <div class="col-md-6">
-          <div class="mb-1"><strong>Gender:</strong> Male</div>
-        </div>
-      </div>
+        const htmlHead = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>User Information</title>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+        </head>
+        <body>
+        <div class="container mt-2 small">
+        `;
 
-      <h2 class="text-primary mt-2">
-        Verification Details
-        <span class="text-muted fs-6">(Verified at: 02/04/2025)</span>
-      </h2>
-
-      <div class="row">
-        <!-- PAN Card Details -->
-        <div class="col-md-4 mb-3" id="pan_response">
-          <div class="p-3 shadow-sm rounded bg-light">
-            <h5 class="fw-bold text-dark mb-2">PAN</h5>
-            <div class="mb-1"><strong>Full Name:</strong> Demo User</div>
-            <div class="mb-1"><strong>PAN Number:</strong> ABC123456</div>
-            <div class="mb-1"><strong>Type:</strong> Individual</div>
-          </div>
+        const candidateInfo = `
+        <h2 class="text-primary">Candidate Information</h2>
+        <div class="row g-1">
+          <div class="col-md-6"><div class="mb-1"><strong>Full Name:</strong> ${userName}</div></div>
+          <div class="col-md-6"><div class="mb-1"><strong>Date Of Birth:</strong> ${formattedDob}</div></div>
+          <div class="col-md-6"><div class="mb-1"><strong>Phone Number:</strong> ${phone}</div></div>
+          <div class="col-md-6"><div class="mb-1"><strong>Email:</strong> ${Email}</div></div>
+          <div class="col-md-6"><div class="mb-1"><strong>Address:</strong> ${user.candidate_address}</div></div>
+          <div class="col-md-6"><div class="mb-1"><strong>Gender:</strong> ${Gender}</div></div>
         </div>
+        `;
+        console.log("Gender:", Gender);
 
-        <!-- Passport Details -->
+        const verificationDetails = `
+        <h2 class="text-primary mt-2">
+          Verification Details
+          <span class="text-muted fs-6">(Verified at: ${verification_time})</span>
+        </h2>
+        `;
+
+        const panDetails = `
+  <div class="col-md-4 mb-3" id="pan_response">
+    <div class="p-3 shadow-sm rounded bg-light">
+      <h5 class="fw-bold text-dark mb-2">PAN</h5>
+      <div class="mb-1"><strong>Full Name:</strong> ${
+        user?.pan_response?.result?.user_full_name ?? "N/A"
+      }</div>
+      <div class="mb-1"><strong>PAN Number:</strong> ${
+        user?.pan_response?.result?.pan_number ?? "N/A"
+      }</div>
+      <div class="mb-1"><strong>Type:</strong> ${
+        user?.pan_response?.result?.pan_type ?? "N/A"
+      }</div>
+    </div>
+  </div>
+`;
+
+        const passportDetails = `
         <div class="col-md-4 mb-3" id="passport_response">
           <div class="p-3 shadow-sm rounded bg-light">
             <h5 class="fw-bold text-dark mb-2">PASSPORT</h5>
-            <div class="mb-1"><strong>Full Name:</strong> Demo User</div>
-            <div class="mb-1"><strong>Number:</strong> ABC1234567890</div>
-            <div class="mb-1"><strong>Applied Date:</strong> 13/08/2020</div>
+            <div class="mb-1"><strong>Full Name:</strong> ${
+              user?.passport_response?.result?.name_on_passport ?? "N/A"
+            }</div>
+            <div class="mb-1"><strong>Number:</strong> ${
+              user?.passport_response?.result?.passport_number ?? "N/A"
+            }</div>
+            <div class="mb-1"><strong>Applied Date:</strong> 
+      ${user?.passport_response?.result?.passport_applied_date ?? "N/A"}
+            </div>
           </div>
         </div>
+        `;
 
-        <!-- Aadhaar Details -->
+        const aadhaarDetails = `
         <div class="col-md-4 mb-3" id="aadhaar_response">
           <div class="p-3 shadow-sm rounded bg-light">
             <h5 class="fw-bold text-dark mb-2">Aadhaar</h5>
-            <div class="mb-1"><strong>Aadhaar Number:</strong> 123456</div>
-            <div class="mb-1"><strong>State:</strong> West Bengal</div>
-            <div class="mb-1"><strong>Gender:</strong> Male</div>
+            <div class="mb-1"><strong>Aadhaar Number:</strong> ${
+              user?.aadhaar_response?.result?.user_aadhaar_number ?? "N/A"
+            }</div>
+            <div class="mb-1"><strong>State:</strong>${
+              user?.aadhaar_response?.result?.state ?? "N/A"
+            }</div>
+            <div class="mb-1"><strong>Gender:</strong>${
+              user?.aadhaar_response?.result?.user_gender ?? "N/A"
+            }</div>
           </div>
         </div>
+        `;
 
+        const drivingLicenseDetails = `
         <div class="col-md-6 mb-3" id="dl_response">
           <div class="p-3 shadow-sm rounded bg-light">
-            <div class="d-flex align-items-center mb-2">
-              <h5 class="fw-bold text-dark mb-0 me-2">Driving License</h5>
-            </div>
-            <div class="mt-2">
-              <div class="row">
-                <div class="col-md-6 text-center mb-2"></div>
-                <div class="col-md-6 p-3">
-                  <div class="row">
-                    <div class="col-md-12 mb-2">
-                      <strong>Full Name:</strong> N/A
-                    </div>
-                    <div class="col-md-12 mb-2">
-                      <strong>DL Number:</strong> N/A
-                    </div>
-                    <div class="col-md-12 mb-2">
-                      <strong>Date of Birth:</strong> N/A
-                    </div>
-                  </div>
-                </div>
+            <h5 class="fw-bold text-dark mb-2">Driving License</h5>
+            <div class="row mt-2">
+              <div class="col-md-6">
+                <h6 class="fw-bold">Permanent Address</h6>
+                <p class="small">
+                  <strong>Address:</strong> abcd <br/>
+                  <strong>District:</strong> KOLKATA <br/>
+                  <strong>State:</strong> West Bengal <br/>
+                  <strong>Pin Code:</strong> 700156 <br/>
+                  <strong>Country:</strong> India <br/>
+                </p>
               </div>
-              <div class="row mt-2">
-                <div class="col-md-6 mb-2">
-                  <strong>Father/Husband Name:</strong> N/A
-                </div>
-                <div class="col-md-6 mb-2">
-                  <strong>License State:</strong> N/A
-                </div>
-                <div class="col-md-6 mb-2">
-                  <strong>License Status:</strong> N/A
-                </div>
-                <div class="col-md-6 mb-2">
-                  <strong>Blood Group:</strong> N/A
-                </div>
-                <div class="col-md-6 mb-2">
-                  <strong>Expiry Date:</strong> N/A
-                </div>
-                <div class="col-md-6 mb-2">
-                  <strong>Issued Date:</strong> N/A
-                </div>
-              </div>
-              <div class="row mt-2">
-                <div class="col-md-6">
-                  <h6 class="fw-bold">Permanent Address</h6>
-                  <p class="small">
-                    <strong>Address:</strong> abcd <br />
-                    <br />
-                    <strong>District:</strong>KOLKATA <br />
-                    <strong>State:</strong> West Bengal <br />
-                    <strong>Pin Code:</strong> 700156<br />
-                    <strong>Country:</strong> India <br />
-                  </p>
-                </div>
-                <div class="col-md-6">
-                  <h6 class="fw-bold">Present Address</h6>
-                  <p class="small">
-                    <strong>Address:</strong> abcd <br />
-                    <br />
-                    <strong>District:</strong>KOLKATA <br />
-                    <strong>State:</strong> West Bengal <br />
-                    <strong>Pin Code:</strong> 700156<br />
-                    <strong>Country:</strong> India <br />
-                  </p>
-                </div>
+              <div class="col-md-6">
+                <h6 class="fw-bold">Present Address</h6>
+                <p class="small">
+                  <strong>Address:</strong> abcd <br/>
+                  <strong>District:</strong> KOLKATA <br/>
+                  <strong>State:</strong> West Bengal <br/>
+                  <strong>Pin Code:</strong> 700156 <br/>
+                  <strong>Country:</strong> India <br/>
+                </p>
               </div>
             </div>
           </div>
         </div>
+        `;
 
+        const epicDetails = `
         <div class="col-md-6 mb-3" id="epic_response">
           <div class="p-3 shadow-sm rounded bg-light">
-            <div class="d-flex align-items-center mb-2">
-              <h5 class="fw-bold text-dark mb-0 me-2">EPIC</h5>
+            <h5 class="fw-bold text-dark mb-2">EPIC</h5>
+            <div class="mb-2">
+              <span class="fw-bold me-2">EPIC ID:</span> 1519865419864984<br/>
+              <span class="fw-bold me-2">Name:</span> Demo name<br/>
+              <span class="fw-bold me-2">Gender:</span> Male<br/>
+              <span class="fw-bold me-2">Age:</span> 25<br/>
+              <span class="fw-bold me-2">Status:</span> Active<br/>
+              <span class="fw-bold me-2">Relative Name:</span> Demo name relative<br/>
             </div>
-            <div class="mt-2">
-              <div class="mb-2">
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">EPIC ID:</span>
-                  <span class="text-break"> 1519865419864984 </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Name:</span>
-                  <span class="text-break"> Demo name </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Gender:</span>
-                  <span class="text-break"> Male </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Age:</span>
-                  <span class="text-break"> 25 </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Status:</span>
-                  <span class="text-break">Active</span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2"> Relative Name: </span>
-                  <span class="text-break"> Demo name relative </span>
-                </div>
-              </div>
-
-              <div class="mb-2">
-                <h5 class="fw-bold">Location Details</h5>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">District:</span>
-                  <span class="text-break"> Kolkata (123) </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">State:</span>
-                  <span class="text-break"> West Bengal (123) </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Assembly Constituency:</span>
-                  <span class="text-break">
-                    Demo Assembly Constituency (123)
-                  </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Parliamentary Constituency:</span>
-                  <span class="text-break">
-                    Demo Parliamentary Constituency (123456)
-                  </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Constituency Part Name:</span>
-                  <span class="text-break">
-                    Demo Constituency Part Name (123)
-                  </span>
-                </div>
-              </div>
-              <div class="mb-2">
-                <h5 class="fw-bold">Polling Booth Details</h5>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Polling Booth:</span>
-                  <span class="text-break"> Demo Polling Booth (123) </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Location:</span>
-                  <span class="text-break"> Kolkata </span>
-                </div>
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">
-                    Serial Number Applicable Part:
-                  </span>
-                  <span class="text-break"> 123 </span>
-                </div>
-
-                <div class="d-flex align-items-center mb-1">
-                  <span class="fw-bold me-2">Last Updated:</span>
-                  <span class="text-break"> 13/08/2020 </span>
-                </div>
-              </div>
+            <h5 class="fw-bold">Location Details</h5>
+            <div class="mb-2">
+              <span class="fw-bold me-2">District:</span> Kolkata (123)<br/>
+              <span class="fw-bold me-2">State:</span> West Bengal (123)<br/>
+              <span class="fw-bold me-2">Assembly Constituency:</span> Demo Assembly Constituency (123)<br/>
+              <span class="fw-bold me-2">Parliamentary Constituency:</span> Demo Parliamentary Constituency (123456)<br/>
+              <span class="fw-bold me-2">Constituency Part Name:</span> Demo Constituency Part Name (123)<br/>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+        `;
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
-  `;
+        const htmlEnd = `
+        </div> <!-- end of .row -->
+        </div> <!-- end of .container -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        </body>
+        </html>
+        `;
+
+        const htmlContent = `
+        ${htmlHead}
+        ${candidateInfo}
+        ${verificationDetails}
+        <div class="row">
+          ${panDetails}
+          ${passportDetails}
+          ${aadhaarDetails}
+          ${drivingLicenseDetails}
+          ${epicDetails}
+        </div>
+        ${htmlEnd}
+        `;
 
         // Header Template (Proper Puppeteer Formatting)
         const headerTemplate = `
@@ -296,7 +235,11 @@ export const generatePDF = async (req, res) => {
         // Footer Template (Fixes Page Number Issue)
         const footerTemplate = `
           <div style="width: 100%; text-align: center; padding: 5px; font-size: 10px; border-top: 1px solid #ccc;">
+            <p style="font-size: 10px; text-align: right; margin: 0;">Verified by QuikCheck using Digilocker</p>
             Page <span class="pageNumber"></span> of <span class="totalPages"></span>
+          </div>
+          <div>
+          
           </div>
         `;
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,78 +1,81 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-name: {
-type: String,
-required: true,
-},
-password: {
-type: String,
-required: true,
-},
-profilePicture: {
-type: String,
-},
-email: {
-type: String,
-required: true,
-unique: true,
-},
-phone_number: {
-type: String,
-},
-address: {
-type: String,
-},
-gst_no: {
-type: String,
-},
-package_id: {
-type: String,
-},
-allowed_verifications :{
-type: String,
-},
-discount_percent: {
-type: String,
-},   
-gender: {
-type: String,
-},
-transaction_fee: {
-type: String,
-},
-transaction_gst: {
-type: String,
-},
-createdAt: {
-type: Date,
-default: Date.now,
-},
-updatedAt: {
-type: Date,
-default: Date.now,
-},
-is_del: {
-type: Boolean,
-default: false,
-},
-is_active: {
-type: Boolean,
-default: true,
-},
-wallet_amount: {
-type: Number,
-default: 0,
-},
-role: {
-/* 0 for admin ,1 for candidate ,2 for company  */
-type: Number,
-required: true,
-default: 1,
-}
-}, {
-timestamps: true,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone_number: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    gst_no: {
+      type: String,
+    },
+    package_id: {
+      type: String,
+    },
+    allowed_verifications: {
+      type: String,
+    },
+    discount_percent: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+    transaction_fee: {
+      type: String,
+    },
+    transaction_gst: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    is_del: {
+      type: Boolean,
+      default: false,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    wallet_amount: {
+      type: Number,
+      default: 0,
+    },
+    role: {
+      /* 0 for admin ,1 for candidate ,2 for company  */
+      type: Number,
+      required: true,
+      default: 1,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
