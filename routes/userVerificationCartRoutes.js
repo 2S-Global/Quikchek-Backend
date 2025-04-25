da36d0457256157dd4af239aa33fe9cf4c9537c6
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-25 12:10:54 +0530

    Update userVerificationCartRoutes.js

--- routes/userVerificationCartRoutes.js
+++ routes/userVerificationCartRoutes.js
@@ -35,7 +35,13 @@ const userRouter = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-userRouter.post('/add_user_cart', upload.none(), userAuth, Companymid, addUserToCart);
+userRouter.post('/add_user_cart',  upload.fields([
+    { name: 'pandoc', maxCount: 1 },
+    { name: 'aadhaardoc', maxCount: 1 },
+    { name: 'licensenumdoc', maxCount: 1 },
+    { name: 'passportdoc', maxCount: 1 },
+    { name: 'voterdoc', maxCount: 1 }
+  ]), userAuth, Companymid, addUserToCart);
 userRouter.get('/list_user_cart', upload.none(), userAuth, Companymid, getUserVerificationCartByEmployer);
 userRouter.get('/list_user_cart_all', upload.none(), userAuth, Companymid, getUserVerificationCartByEmployerAll);
 userRouter.post('/getPaidUserVerificationCartByEmployer', upload.none(), userAuth, Companymid, getPaidUserVerificationCartByEmployer);
