ac9e88d4781b91ad23f673b87b5926b746ea62ee
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-30 16:55:37 +0530

    push

---
+++ models/userVerificationCartAadhatOTPModel.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const userVerificationCartAadharOTPSchema = new mongoose.Schema(
+    {
+        employer_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        candidate_name: {
+            type: String,
+            required: true,
+        },
+        candidate_email: {
+            type: String,
+            required: true,
+        },
+        candidate_mobile: {
+            type: String,
+        },
+        candidate_dob: {
+            type: String,
+            required: true,
+        },
+        candidate_address: {
+            type: String,
+        },
+        candidate_gender: {
+            type: String, // Fixed typo
+        },
+        
+        aadhar_name: {
+            type: String, // Fixed typo
+        },
+        aadhar_number: {
+            type: String, // Fixed typo
+        },
+        aadhar_image: {
+            type: String, // Ensure marks is stored properly
+        },   
+        
+        aadhat_otp: {
+            type: String,
+            default: 'no',
+          },
+        updatedAt: {
+            type: Date,
+            default: Date.now,
+        },
+        is_paid: { 
+            type: Number, 
+            default: 0 
+        },
+
+        is_del: {
+            type: Boolean,
+            default: false,
+        },
+        all_verified:{
+             type: Number,
+            default: 0,
+    }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const UserCartVerificationAadhatOTP = mongoose.model("UserVerificationCartAadhatOTP", userVerificationCartAadharOTPSchema);
+
+export default UserCartVerificationAadhatOTP;
