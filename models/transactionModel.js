abd53ed78c11c22ecaeab06d67f7772dd2562ea2
2S-Global <2sglobaltechnologies@gmail.com>
2025-05-16 17:00:55 +0530

    fixed code update issue

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -1,51 +1,51 @@
 import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
-
-    employer_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    order_id: {
-        type: String,
-    },
-    transactionId: {
-        type: String,
-     
-    },
-    amount: {
-        type: Number,
-    },
-    paymentids: {
-        type: String,
-    },
-        payment_method:{
-        type:String,
-        enum:["online","Wallet"]
-
-    },
-    payment_type: {
-        type: String,
-        enum: ["credit", "debit"],
-      
-      },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now,
-    },
-    paymenttime: {
-        type: Date,
-        default: Date.now,
-    },
-    is_del: {
-        type: Boolean,
-        default: false,
-    },
+  employer_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  order_ref_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "allOrdersData",
+  },
+  order_id: {
+    type: String,
+  },
+  transactionId: {
+    type: String,
+  },
+  amount: {
+    type: Number,
+  },
+  paymentids: {
+    type: String,
+  },
+  payment_method: {
+    type: String,
+    enum: ["online", "Wallet"],
+  },
+  payment_type: {
+    type: String,
+    enum: ["credit", "debit"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  paymenttime: {
+    type: Date,
+    default: Date.now,
+  },
+  is_del: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
