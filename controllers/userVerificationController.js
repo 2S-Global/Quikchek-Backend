e6404b4313303b2d9110bca6456e77123e3d224b
2S-Global <2sglobaltechnologies@gmail.com>
2025-08-01 15:47:33 +0530

    Refactor OTP Aadhar verification logic to streamline response handling and remove redundant checks

--- controllers/userVerificationController.js
+++ controllers/userVerificationController.js
@@ -1605,21 +1605,6 @@ export const verifyOtpAadhar = async (req, res) => {
     );
 
     console.log("OTP Verification Response:", response.data);
-    console.log("code", response.data.status_code);
-
-    if (response.data.status_code !== 200) {
-      return res
-        .status(200)
-        .json({ success: false, message: response.data.message });
-    }
-
-    if (!response.data.success) {
-      return res.status(400).json({
-        success: false,
-        message: response.data.message || "OTP verification failed",
-        data: response.data,
-      });
-    }
 
     // If OTP is successfully verified, update the document in UserVerification collection
     const updatedDoc = await UserVerification.findByIdAndUpdate(
