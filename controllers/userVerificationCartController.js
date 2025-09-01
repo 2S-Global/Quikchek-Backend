84b69478f5d546b680c008b3299234042007ac49
Chandra-Sarkar <chandrasarkar26345@gmail.com>
2025-09-01 18:41:46 +0530

    update

--- controllers/userVerificationCartController.js
+++ controllers/userVerificationCartController.js
@@ -392,7 +392,7 @@ export const addUserToCartAadharOTP = async (req, res) => {
     if (user.role === 3) {
       if (user.freeVerificationUsed) {
         // ❌ Already used free trial
-        return res.status(403).json({
+        return res.status(404).json({
           success: false,
           message:
             "Your free trial verification has already been used. Please purchase to continue.",
